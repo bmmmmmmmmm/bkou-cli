7fc78306953c79dad737a14d974e3552f0740d36
Benming Kou <koubm@300624.cn>
2024-11-30 19:17:22 +0800

    DEL: sth move to BKit

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,7 @@
 import { _log } from "./log";
 import { _isAbsolutePath, _isRelativePath } from "./path";
-import { _createMap } from "./common";
 
 export {
   _log,
-  _isAbsolutePath, _isRelativePath,
-  _createMap
+  _isAbsolutePath, _isRelativePath
 }
