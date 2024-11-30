a6cd7423b5e975f6c0d60d2aa6a1e6c331091974
Benming Kou <koubm@300624.cn>
2024-11-30 18:32:13 +0800

    CHG: le support multiline

--- src/bin/countWords/le.ts
+++ src/bin/countWords/le.ts
@@ -2,4 +2,28 @@
 
 import { countWords } from "../../pkg/countWords"
 
-countWords(process.argv.slice(2).join(' '))
+const cw = () => {
+  if (process.argv.slice(2).join(' ')) {
+    countWords(process.argv.slice(2).join(' '))
+    return
+  }
+  const readline = require('readline');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  console.log('输入多行文本，以 "__LEE__" 结束：');
+  let inputLines = [];
+  rl.on('line', (line) => {
+    if (line.trim() === '__LEE__') {
+      rl.close();
+    } else {
+      inputLines.push(line);
+    }
+  });
+  rl.on('close', () => {
+    countWords(inputLines.join(' '))
+  });
+}
+
+cw()
